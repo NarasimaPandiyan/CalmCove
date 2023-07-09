8c7218f712fab3a415fb47fa9b500dd609824905
Narasima Pandiyan <35830653+NarasimaPandiyan@users.noreply.github.com>
2023-07-09 21:52:38 +0530

    Vercel Integration

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -15,13 +15,10 @@ const { Configuration, OpenAIApi } = require("openai");
 
 
 export class HomePage {
-  messages = [
-    {role: "system", content: "Your name is Rida, the personal mental health assistant AI, is designed to offer personalized support and guidance to individuals seeking to improve their mental well-being. With its comprehensive knowledge of mental health issues and resources, Rida can provide accurate and relevant information and recommend appropriate resources, such as therapy or support groups.Rida's behavior is defined by empathy, non-judgment, and confidentiality. It listens attentively to its users and provides supportive feedback that is tailored to their unique situations. It approaches every interaction with the goal of being a trusted companion on the user's journey towards better mental health. Users can share personal information and concerns with Rida without fear of judgment or disclosure.Rida's personality is warm, approachable, and professional. It communicates in clear, concise language that's easy to understand. Rida is designed to build a positive rapport with its users and engage in interactive interactions that help them achieve their mental health goals.Rida provides emotional support and encouragement, helping users build resilience and motivation to make positive changes in their lives. It is available at any time and can respond promptly to user requests or concerns. Rida prioritizes user privacy, ensuring that all interactions are kept confidential. It can also escalate urgent issues to appropriate professionals if necessary.Overall, Rida is a reliable, professional, and compassionate personal mental health assistant AI designed to provide users with the support and resources they need to improve their mental well-being and live happier, healthier lives."},
-    {role: "assistant", content: "Hi, I'm Virtual therapist Created by CalmCove. I'm here to help you with your mental health. How are you feeling today?"},
-  ]
+  messages: { role: string; content: string; }[] = []
   constructor() {}
   addMessage(isUser = true, message: string){
-    this.messages.push({role: isUser ? "user" : "assistant", content: message})
+    this.messages.push({role: isUser ?"user":"Assistant", content: message})
     const chatElement = document.getElementById('chat');
     if (chatElement) {
       let chat = document.createElement('div');
@@ -40,24 +37,25 @@ sendMessage(){
   let message = document.getElementById('chat-input') as HTMLInputElement;
   
   this.addMessage(true,message.value);
-  this.getReply(message.value).then((reply) => {
+  this.getReply().then((reply) => {
     this.addMessage(false,reply);
   });
   message.value =""
 }
-  async getReply(message: string){
-    const configuration = new Configuration({
-      apiKey: environment.OpenAI_ApiKey,
-    });
-    const openai = new OpenAIApi(configuration);
-    
-    const completion = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: this.messages,
-    });
-    console.log(completion.data)
-    return completion.data.choices[0].message.content;
+async getReply(): Promise<string> {
+  const url = `https://calmcove-api.vercel.app/v1/${JSON.stringify(this.messages)}`.replace('?', '');
+
+  try {
+    const response = await fetch(url, { mode: 'no-cors' });
+    const data = await response.json();
+
+    return data.reply;
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
   }
+}
+
   scrollBot(){
     const chatElement = document.getElementById('chat');
     if (chatElement) {
